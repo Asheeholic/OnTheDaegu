da2d5833cd099fd67f61c1536d2193c0010c0a1b
Han Jae Ho <chacha_95@naver.com>
2021-08-10 15:32:58 +0900

    initial commit - jaeho

---
+++ otd/src/main/java/co/yedam/otd/web/FrontController.java
@@ -0,0 +1,50 @@
+package co.yedam.otd.web;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import co.yedam.otd.common.Command;
+import co.yedam.otd.common.HomeCommand;
+
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private HashMap<String, Command> map = new HashMap<String, Command>();   
+
+    public FrontController() {
+        super();
+    }
+
+
+	public void init(ServletConfig config) throws ServletException {
+		map.put("/home.do", new HomeCommand());  //메인페이지
+		
+	}
+
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String uri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String path = uri.substring(contextPath.length());
+		
+		Command command = map.get(path);
+		String viewPage = command.execute(request, response);
+		
+		if(!viewPage.endsWith(".do")) {
+			viewPage = "WEB-INF/views/" + viewPage + ".jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+
+}
